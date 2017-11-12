360ecf8ad392d885a81499a08e58e72d94ed214a
suguannan <guannan.su@net263.com>
2017-11-12 18:56:15 +0800

    add template and request

--- js/index.js
+++ js/index.js
@@ -2,28 +2,43 @@
  * Created by Elaine on 10/11/2017.
  */
 
-$(document).ready(function(){
+$(function () {
+    console.log(111111)
+    Vote.getActivity(getActivityDone, getActivityFailure)
+    Vote.getContestants(getContestantsDone, getContestantsFailure)
+
+
+
+
+    // Session
+    // if (!dfData.getSessionData("isFirstRun")) {
+    //     dfData.setSessionData("isFirstRun", "true");
+    //     $("#welcome").show();
+    // }
+    
     // tab切换
     $('#tabWrapper li').click(function (e) {
         $('#tabWrapper li').css('background','#ffc213');
         $(e.target).css('background','#393a9b');
     })
-    // 调用全部参赛者接口 渲染数据
 
 
 
-/* tab控制 **************************************据说这是一个很不错的选择*/
+/* tab控制 ***************************************/
     // 全、部参赛者
     $('#allBtn').click(function (e) {
         $(".content").hide();
         $("#contentMain").show();
         // 调用全部参赛者接口 渲染数据
+        Vote.getContestants(getContestantsDone, getContestantsFailure)
 
     })
     // 当前排名
     $('#bankingBtn').click(function (e) {
         $(".content").hide();
         $("#contentRanking").show();
+        // 调用排名接口
+        Vote.getRanking(getRankingDone, getRankingFailure)
     })
     // 活动规则
     $('#ruleBtn').click(function (e) {
@@ -37,11 +52,80 @@ $(document).ready(function(){
             // 调用搜索接口
         }
     })
+    // 投票
+    $('#voteBtn').click(function (e) {
+            // 调用投票接口
+            // 参数id
+            var id = 1;
+            Vote.getGiveVote(id, getGiveVoteDone, getGiveVoteFailure)
+    })
     // 点击进详情
     $("contestantWrapper").on("click", "li", function(){
         $("contestantWrapper").hide();
         $("detailWrapper").show();
         // 调用详情接口
+        Vote.getDetails(getDetailsDone, getDetailsFailure)
         // 渲染详情页数据
     })
+    
+    /* ******************callback function*********************** */
+    /* getActivity */
+    function getActivityDone(data) {
+        logd('getActivity success');
+        logd('Activity Data:' + JSON.stringify(data));
+        var html = template('tep_activity', data);
+        document.getElementById('activity').innerHTML = html;
+    }
+
+    function getActivityFailure(data) {
+        logd('getActivity fail');
+    }
+
+    /* getContestants */
+    function getContestantsDone(data) {
+        logd('getContestants success');
+        console.log(data)
+        var html = template('tep_contestantWrapper', data);
+        document.getElementById('contestantWrapper').innerHTML = html;
+    }
+
+    function getContestantsFailure(data) {
+        logd('getActivity fail');
+    }
+
+    /* getDetails */
+    function getDetailsDone(data) {
+        logd('getDetails success');
+        logd('getDetails Data:' + JSON.stringify(data));
+        // var html = template('tep_activity', data);
+        // document.getElementById('activity').innerHTML = html;
+    }
+
+    function getDetailsFailure(data) {
+        logd('getDetails fail');
+    }
+
+    /* getRanking */
+    function getRankingDone(data) {
+        logd('getRanking success');
+        logd('getRanking Data:' + JSON.stringify(data));
+        // var html = template('tep_activity', data);
+        // document.getElementById('activity').innerHTML = html;
+    }
+
+    function getRankingFailure(data) {
+        logd('getRanking fail');
+    }
+
+    /* getGiveVote */
+    function getGiveVoteDone(data) {
+        logd('getGiveVote success');
+        logd('getGiveVote Data:' + JSON.stringify(data));
+        // var html = template('tep_activity', data);
+        // document.getElementById('activity').innerHTML = html;
+    }
+
+    function getGiveVoteFailure(data) {
+        logd('getGiveVote fail');
+    }
 });
---
+++ js/request.js
@@ -0,0 +1,128 @@
+/**
+ * Created by Elaine on 2017/11/12 15:33.
+ *
+ */
+
+/**
+ * Error codes.
+ */
+// var VOTEDdata_ERROR_UNKNOWN = -1;
+
+
+var VOTE_URL_BASE = "http://192.168.20.203:5555/";
+// var VOTE_URL_BASE = "http://192.168.20.203:5555/vote";
+
+
+/* public information */
+var logi = function(str) {
+    console.log("[ElaineLog-I ----] " + str);
+}
+
+/* debug information */
+var logd = function(str) {
+    console.log("[ElaineLog-D ----] " + str);
+}
+
+/* errors */
+var loge = function(str) {
+    console.log("[ElaineLog-E ----] " + str);
+}
+
+var VOTEDdata = (function() {
+    var VoteInstance;
+
+    function createInstance() {
+        init();
+        logi("VOTEDdata instance created.");
+
+        return {
+            getActivity: getActivity,
+            getContestants: getContestants,
+            getDetails: getDetails,
+            getRanking: getRanking,
+            getGiveVote: getGiveVote
+        };
+    }
+
+    var init = function() {
+        // var normalDataVer = getUserData(KEY_NORMAL_DATA_VERSION);
+        // if (!normalDataVer || normalDataVer < NORMAL_DATA_VERSION) {
+        //     removeNormalData();
+        //     setUserData(KEY_NORMAL_DATA_VERSION, NORMAL_DATA_VERSION);
+        // }
+    }
+
+    var makeRequest = function(urlBase, url, param, done, failure) {
+        logd("MakeRequest: url=" + urlBase + url + ", param=" + JSON.stringify(param));
+        $.ajax({
+            type: "POST",
+            url: urlBase + url,
+            data: param,
+            dataType: "text",
+            success: function(data, textStatus, jqXHR) {
+                done(data);
+            },
+            error: function(jqXHR, textStatus, errorThrown) {
+                var errorCode = parseInt(jqXHR.responseText);
+                loge("MakeRequest: failed, errorCode=" + errorCode);
+                failure(errorCode);
+            }
+        });
+    }
+
+
+
+    /**
+     * Get activity data.
+     */
+    var getActivity = function(done, failure) {
+        makeRequest(VOTE_URL_BASE, "activity", {}, done, failure);
+    }
+
+    /**
+     * Get contestants data.
+     */
+    var getContestants = function(done, failure) {
+        makeRequest(VOTE_URL_BASE, "query/json", {},done, failure);
+    }
+
+    /**
+     * Get details of contestants.
+     */
+    var getDetails = function(done, failure) {
+        makeRequest(VOTE_URL_BASE, "detail/{id}", {}, done, failure);
+    }
+
+    /**
+     * Get ranking.
+     */
+    var getRanking = function(done, failure) {
+        makeRequest(VOTE_URL_BASE, "topVote", {},done, failure);
+    }
+
+    /**
+     * Give Vote.
+     */
+    var getGiveVote = function(id, done, failure) {
+        makeRequest(VOTE_URL_BASE, "giveVote/" + id, {},done, failure);
+    }
+
+    return {
+        /**
+         * Get the singleton instance.
+         */
+        getInstance: function() {
+            if (!VoteInstance) {
+                VoteInstance = createInstance();
+            }
+            return VoteInstance;
+        }
+    };
+
+})();
+
+
+
+
+var Vote = VOTEDdata.getInstance();
+
